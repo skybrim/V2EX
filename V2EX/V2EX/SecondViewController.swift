1ee1eb684d4f5ff39029474fe272632b2465bb6a
Wiley <throughskybrim@gmail.com>
2019-05-06 14:42:07 +0800

    new project

---
+++ V2EX/V2EX/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  V2EX
+//
+//  Created by Wiley on 2019/5/6.
+//  Copyright © 2019 Wiley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
