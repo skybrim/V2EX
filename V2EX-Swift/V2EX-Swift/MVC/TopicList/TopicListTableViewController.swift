da8400a18ad20ba416a81cc029e1b069edf1a55c
Wiley <throughskybrim@gmail.com>
2019-07-02 14:44:27 +0800

    .#

--- V2EX-Swift/V2EX-Swift/MVC/TopicList/TopicListTableViewController.swift
+++ V2EX-Swift/V2EX-Swift/MVC/TopicList/TopicListTableViewController.swift
@@ -14,23 +14,15 @@ class TopicListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.topicList.topics.count
     }
     
@@ -44,57 +36,12 @@ class TopicListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     //MARK: - Network
     
-    /// 根据tab名称，请求数据。解析html
+    /// 根据tab名称，请求数据，并解析html
     ///
     /// - Parameter tab: theme
     private func requestTopicList(tab: String) {
@@ -139,12 +86,4 @@ class TopicListTableViewController: UITableViewController {
     }
     
     var topicList = TopicList()
-    
-    @IBOutlet var topicListTableView: UITableView! {
-        didSet {
-//            topicListTableView.register(TopicListTableViewCell.self, forCellReuseIdentifier: "Topic Cell")
-//            topicListTableView.rowHeight = UITableView.automaticDimension
-//            topicListTableView.estimatedRowHeight = 70
-        }
-    }
 }
