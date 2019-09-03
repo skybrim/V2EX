8fca73f77bc2510612ac394008377f982e36508c
skybrim <throughskybrim@gmail.com>
2019-09-03 15:23:07 +0800

    pod quick

---
+++ V2EX/V2EXTests/QuickTests.swift
@@ -0,0 +1,18 @@
+//
+//  QuickTests.swift
+//  V2EXTests
+//
+//  Created by wiley on 2019/9/3.
+//  Copyright © 2019 wiley. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import V2EX
+
+class V2EXQuickTests: QuickSpec {
+    override func spec() {
+        
+    }
+}
